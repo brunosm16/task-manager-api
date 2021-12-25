54bb4c876508e27dd9e006b186640a13a5da76e6
brunosm16 <brunoskr23@gmail.com>
2021-12-25 20:02:09 -0300

    feat: ensure TaskController handle implements interfaces

--- src/presentation/controllers/task/task.ts
+++ src/presentation/controllers/task/task.ts
@@ -1,6 +1,7 @@
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { HttpRequest, HttpResponse } from '../../protocols/http'
+
 export class TaskController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return { statusCode: 400, body: new Error('Missing Param: name') }
     }
@@ -8,5 +9,7 @@ export class TaskController {
     if (!httpRequest.body.completed) {
       return { statusCode: 400, body: new Error('Missing Param: completed') }
     }
+
+    return { statusCode: 404, body: new Error('Not Found') }
   }
 }
