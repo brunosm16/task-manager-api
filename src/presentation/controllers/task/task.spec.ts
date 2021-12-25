f82aaf4d6132debd19dce0191c40e6ac5d766b9a
brunosm16 <brunoskr23@gmail.com>
2021-12-25 18:26:36 -0300

    test: ensure TaskController returns 400 if no name is provided

---
+++ src/presentation/controllers/task/task.spec.ts
@@ -0,0 +1,17 @@
+import { TaskController } from './task'
+
+describe('Task Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new TaskController()
+
+    const httpRequest = {
+      body: {
+        completed: true
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
