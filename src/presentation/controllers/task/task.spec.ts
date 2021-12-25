239595c4b5a71c3ec22bba102f0ded52de2b4528
brunosm16 <brunoskr23@gmail.com>
2021-12-25 19:29:06 -0300

    test: ensure TaskController returns 400 and an Error if no completed is provided

--- src/presentation/controllers/task/task.spec.ts
+++ src/presentation/controllers/task/task.spec.ts
@@ -15,4 +15,19 @@ describe('Task Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing Param: name'))
   })
+
+  test('Should return 400 if no completed is provided', () => {
+    const sut = new TaskController()
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing Param: completed'))
+  })
 })
