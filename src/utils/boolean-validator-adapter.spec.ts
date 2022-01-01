6c472c682b3736e1941240520e5a7f1835c092b6
brunosm16 <brunoskr23@gmail.com>
2022-01-01 18:28:58 -0300

    test: ensure BooleanValidatorAdapter returns true if validator returns true

--- src/utils/boolean-validator-adapter.spec.ts
+++ src/utils/boolean-validator-adapter.spec.ts
@@ -1,4 +1,11 @@
 import { BooleanValidatorAdapter } from './boolean-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isBoolean (completed: true): boolean {
+    return true
+  }
+}))
 
 const makeSut = (): BooleanValidatorAdapter => {
   return new BooleanValidatorAdapter()
@@ -8,11 +15,20 @@ describe('BooleanValidatorAdapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = makeSut()
 
-    jest.spyOn(sut, 'isValid').mockReturnValueOnce(false)
+    jest.spyOn(validator, 'isBoolean').mockReturnValueOnce(false)
 
     const completed = true
     const result = sut.isValid(completed.toString())
 
     expect(result).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = makeSut()
+
+    const completed = true
+    const result = sut.isValid(completed.toString())
+
+    expect(result).toBe(true)
   })
 })
