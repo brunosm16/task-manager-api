15856b32e588fe212c790bdf83d5fb8cdcad32a3
brunosm16 <brunoskr23@gmail.com>
2022-01-03 12:13:46 -0300

    test: ensure NameValidatorAdapter return true if validator returns true

--- src/utils/name-validator-adapter.spec.ts
+++ src/utils/name-validator-adapter.spec.ts
@@ -1,26 +1,27 @@
-import validator from 'validator'
 import { NameValidatorAdapter } from './name-validator-adapter'
-
-jest.mock('validator', () => ({
-  isNumeric (value: string) {
-    return true
-  }
-}))
 
 const makeSut = (): NameValidatorAdapter => {
   return new NameValidatorAdapter()
 }
 
 describe('Name Validator', () => {
-  test('Should return false if isNumeric returns false', () => {
+  test('Should return false if validator returns false', () => {
     const sut = makeSut()
-
-    jest.spyOn(validator, 'isNumeric').mockReturnValueOnce(false)
 
     const name = 123
 
     const result = sut.isValid(name.toString())
 
     expect(result).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = makeSut()
+
+    const name = 'abcde'
+
+    const result = sut.isValid(name.toString())
+
+    expect(result).toBe(true)
   })
 })
--- src/utils/name-validator-adapter.ts
+++ src/utils/name-validator-adapter.ts
@@ -1,11 +1,11 @@
-import validator from 'validator'
 import { NameValidator } from '../presentation/protocols/name-validator'
 
 export class NameValidatorAdapter implements NameValidator {
   isValid (name: string): boolean {
-    const isString = !validator.isNumeric(name)
-    const isCorrectLength = name.length >= 5 && name.length >= 500
+    const isString = parseInt(name, 10)
 
-    return isString && isCorrectLength
+    const isCorrectLength = name.length >= 4 && name.length <= 500
+
+    return !isString && isCorrectLength
   }
 }
